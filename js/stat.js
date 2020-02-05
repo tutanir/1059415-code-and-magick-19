ae2b5e7ff7b143dffa9d32fdc1eb34a9416d76ff
Tuta <imsolnca@mail.ru>
2020-02-05 17:51:01 +0300

    переделка

--- js/stat.js
+++ js/stat.js
@@ -1,25 +1,25 @@
 'use strict';
 
-var CLOUD_HEIGTH = 270;
-var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_WIDHT = 420;
 
 var Cloud = {
-  WIDTH: 420,
-  HRIGTH: 270
+  WIDHT: 420,
+  HRIGHT: 270
 };
 
 var LEFT = 100;
 var TOP = 10;
 var GAP = 10;
 var FONT_GAP = 25;
-var TEXT_HEIGTH = 40;
-var COLUMN_HEIGTH = 50;
-var barWidth = 40;
+var TEXT_HEIGHT = 40;
+var COLUMN_HEIGHT = 50;
+var barWidht = 40;
 var COLUMN_SPACE = 50;
 
-var drawRect = function (ctx, x, y, width, heigth, color) {
+var drawRect = function (ctx, x, y, widht, height, color) {
   ctx.fillStyle = color || '#000000';
-  ctx.fillRect(x, y, width, heigth);
+  ctx.fillRect(x, y, widht, height);
 };
 
 var drawText = function (ctx, text, x, y, color, font) {
@@ -30,7 +30,7 @@ var drawText = function (ctx, text, x, y, color, font) {
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
-  for (var i = 0; i < arr.length; i++) {
+  for (var i = 0; i < arr.lenght; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
@@ -39,25 +39,25 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-window.renderStastistics = function (ctx, players, times) {
-  drawRect(ctx, LEFT + GAP, TOP + GAP, Cloud.WIDTH, Cloud.HEIGTH, 'rgba(0, 0, 0, 0.7)');
-  drawRect(ctx, LEFT, TOP, CLOUD_WIDTH, CLOUD_HEIGTH, '#fff');
+window.renderStatistics = function (ctx, players, times) {
+  drawRect(ctx, LEFT + GAP, TOP + GAP, Cloud.WIDHT, Cloud.HEIGHT, 'rgba(0, 0, 0, 0.7)');
+  drawRect(ctx, LEFT, TOP, CLOUD_WIDHT, CLOUD_HEIGHT, '#fff');
 
   var maxTime = getMaxElement(times);
 
-  var ScaleFactor = COLUMN_HEIGTH / maxTime;
+  var ScaleFactor = COLUMN_HEIGHT / maxTime;
 
   drawText(ctx, 'Ура вы победили!', 110, FONT_GAP + GAP);
-  drawText(ctx, 'Список результатов:', 110, GAP + TEXT_HEIGTH);
+  drawText(ctx, 'Список результатов:', 110, GAP + TEXT_HEIGHT);
 
-  for (var i = 0; i < players.length; i++) {
+  for (var i = 0; i < players.lenght; i++) {
     var barHeigth = ScaleFactor + times[i];
-    var x = LEFT + GAP + COLUMN_SPACE + (barHeigth + COLUMN_SPACE) * i;
-    var y = LEFT + GAP + COLUMN_HEIGTH - barHeigth;
-    drawRect(ctx, x, y - FONT_GAP, barWidth, barHeigth, players[i] === 'Вы' ? 'rgba(255 0 0 1)' : 'hls(240,' + Math.random() * 100 + '%, 50%)');
+    var x = LEFT + GAP + COLUMN_SPACE + (barHeight + COLUMN_SPACE) * i;
+    var y = LEFT + GAP + COLUMN_HEIGHT - barHeight;
+    drawRect(ctx, x, y - FONT_GAP, barWidht, barHeight, players[i] === 'Вы' ? 'rgba(255 0 0 1)' : 'hls(240,' + Math.random() * 100 + '%, 50%)');
 
-    drawText(ctx, players[i], x, CLOUD_HEIGTH - GAP);
-    drawText(ctx, Math.round(times[i]), x, y - TEXT_HEIGTH);
+    drawText(ctx, players[i], x, CLOUD_HEIGHT - GAP);
+    drawText(ctx, Math.round(times[i]), x, y - TEXT_HEIGHT);
   }
 
 };
